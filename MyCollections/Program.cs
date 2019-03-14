fc6ff866ce8caaf09d05294abd6ad27cf6683703
Jens Lindhardt <jens@lindhardt-freelance.dk>
2019-03-14 11:03:14 +0100

    Add Item Method

--- MyCollections/Program.cs
+++ MyCollections/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SimpleCollections;
 
 namespace MyCollections
 {
@@ -10,6 +10,17 @@ namespace MyCollections
     {
         static void Main(string[] args)
         {
+            LinkedList ll = new LinkedList();
+
+            ll.Add("One");
+            ll.Add("Two");
+            ll.Add("Three");
+            ll.Add("Four");
+            ll.Add("Five");
+
+            ll.PrintList();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ SimpleCollections/LinkedList.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCollections
+{
+    public class LinkedList
+    {
+        private class ListNode
+        {
+            private Object _item;
+
+            public Object Item
+            {
+                get { return _item; }
+                set { _item = value; }
+            }
+
+
+            // Reference to next node in list
+            private ListNode _nextNode = null;
+            public ListNode NextNode
+            {
+                get { return _nextNode; }
+                set { _nextNode = value; }
+            }
+        }
+
+        private ListNode _firstNode = null;
+
+        public Object Add(Object item)
+        {
+            var node = new ListNode();
+            node.Item = item;
+
+            if (_firstNode == null)
+            {
+                _firstNode = node;
+            }
+            else
+            {
+                ListNode currentNode = _firstNode;
+                while (currentNode.NextNode != null )
+                {
+                    currentNode = currentNode.NextNode;
+                }
+
+                currentNode.NextNode = node;
+            }
+            return item;
+        }
+
+        // Test Method remove before release
+        public void PrintList()
+        {
+            ListNode currentNode = _firstNode;
+            while (currentNode.NextNode != null)
+            {
+                Console.WriteLine(currentNode.Item);
+                currentNode = currentNode.NextNode;
+            }
+            Console.WriteLine(currentNode.Item);
+        }
+
+    }
+}
